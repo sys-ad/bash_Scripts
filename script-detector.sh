54364fabb9e84b98f6450981b7bb103afa3bf5c7
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-11 23:05:38 -0500

    Update script-detector.sh

--- script-detector.sh
+++ script-detector.sh
@@ -4,7 +4,15 @@
 # Remember, you might be traversing a directory, a file, a character...
 TESTCHARS=2
 HASHBANG='#!'
-
 for file in *
 do
+if [[ `head c$TESTCHARS '$file'` = $HASHBANG ]]
+then
+echo "File \"$file\" is a script"
+else
+echo "File \"$file\" not a script"
+fi
+done
+
+exit 0
 
