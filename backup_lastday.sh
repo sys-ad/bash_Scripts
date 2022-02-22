e369f2cfb51880fc4b310e37ca0abfeb6c9fb6d9
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-21 23:48:41 -0500

    Update backup_lastday.sh

--- backup_lastday.sh
+++ backup_lastday.sh
@@ -2,3 +2,7 @@
 
 BACKUPFILE=backup-$(date +%m-%d-%Y)
 archive=${1:-$BACKUPFILE}
+
+tar cvf - `find . mtime -1 -type f -print` > $archive.tar
+gzip $archive.tar
+echo "Directory $PWD backedup in archive \"$archive.tar.gz\"."
