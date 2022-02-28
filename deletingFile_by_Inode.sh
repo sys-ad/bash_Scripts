319a3a6689c3fc9f9d67b1da6f909c6f24f67836
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-02-27 23:47:30 -0500

    Update deletingFile_by_Inode.sh

--- deletingFile_by_Inode.sh
+++ deletingFile_by_Inode.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+if [ $# -ne 1 ]    # only takes one argument
+then
+  echo "Usage: `basename $0` filename"
+  exit 71
+fi
+
+if [ ! -e "$1" ]
+then
+  echo "File \""$1"\" does not exist."
+  exit 72
+fi
+
