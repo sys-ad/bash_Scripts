86612bdcda2215e4ce96fb01975c283f095da1d8
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-03-03 23:32:52 -0500

    Update kill_process_by_name.sh

--- kill_process_by_name.sh
+++ kill_process_by_name.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+E_BADARG=66
+
+if test -z "$1" # returns true if string (in this case $1 argument) is an empty " " string
+then
+  echo "Usage: `basename $0` processes to kill"
+  exit $E_BADARG
+fi
+
