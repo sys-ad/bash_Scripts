2326dccf4ce520cd21e8417a3d2a15e09a3c3ecf
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-03-05 23:25:43 -0500

    Create timer.sh

---
+++ timer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+date1=$(date +%s)
+
+#Use this arithmetic to determine elapsed time since defining date1
+#let $(( $(date +%s) - $date1 ))
+
+#While loop using timer
+while [ $(( $(date +%s) - $date1 )) -lt "$1" ]; do
+#Terminal timer -n won't append new line, so the echo will replace itself.
+##There is also some date formatting to achieve: HH:MM:SS.
+echo -ne "$(date -u --date @$(( $(date +%s) - $date1 )) +%H:%M:%S)\r"
+
+done
+sudo ./steelbell.mp3
