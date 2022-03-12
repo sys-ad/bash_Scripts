77777ab12ad1e2b71d3d6f0786dd0e07ffd2517f
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-03-11 23:49:42 -0500

    Update syslog_monitor.sh

--- syslog_monitor.sh
+++ syslog_monitor.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-dddddddddd
+
+filename=sys.log
+
+cat /dev/null > $filename; echo "Cleaning out file.."
+
+tail /var/log/messages > $filename          # Check the read permissions for /var/log/messages, making sure its universal.
+
+echo "$filename contains last entries of system log"
+
+exit 0
