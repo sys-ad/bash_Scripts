036dc69cdf92a1e73707b0fd66f02f089a68dd09
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-03-19 23:41:14 -0400

    Update setNew_Password.sh

--- setNew_Password.sh
+++ setNew_Password.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# Dont consider this a secure script. Requires sudo access. 
+
+echo -n "Enter user name: "
+read USER_NAME
+
+# check if username exist
+
+# invoke useradd, with flags or not
+
+# set password, piping stdin (password) to passwd command
+
+echo "User '$USER_NAME' has been set.
+
+exit 0
+
