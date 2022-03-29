fac90632a150af52c19565505f32f74ee6358ef5
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-03-28 23:46:15 -0400

    Update setNew_Password.sh

--- setNew_Password.sh
+++ setNew_Password.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Dont consider this a secure script. Requires sudo access. 
-
+SUCCESS=0
 echo -n "Enter user name: "
 read USER_NAME
 
 # check if username exist
-if [ -z 
+grep -q "$USER_NAME" /etc/passwd
+if [ z -ne $? ]
+then
+  echo "User $USER_NAME does not exist"
+  exit 70   # Does not exist error, 70
+fi
 
+# Set password
+echo -n "Enter new password: "
+echo 
 # invoke useradd, with flags or not
 
 # set password, piping stdin (password) to passwd command
